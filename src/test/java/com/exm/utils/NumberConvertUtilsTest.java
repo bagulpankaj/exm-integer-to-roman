362341dd900879990c42ed2ba20dd6a2047c820a
Pankaj Bagul <bagulpankaj@yahoo.com>
2022-07-28 21:21:58 +0100

    Added test for 2 disigt integer numbers

--- src/test/java/com/exm/utils/NumberConvertUtilsTest.java
+++ src/test/java/com/exm/utils/NumberConvertUtilsTest.java
@@ -27,4 +27,49 @@ public class NumberConvertUtilsTest {
         String roman9 = NumberConvertUtils.integerToRoman(9);
         assertTrue(roman9.equals("IX"));
     }
+
+
+    @Test
+    public void integerToRomanTwoDigitTest() {
+        String roman10 = NumberConvertUtils.integerToRoman(10);
+        assertTrue(roman10.equals("X"));
+        String roman11 = NumberConvertUtils.integerToRoman(11);
+        assertTrue(roman11.equals("XI"));
+        String roman14 = NumberConvertUtils.integerToRoman(14);
+        assertTrue(roman14.equals("XIV"));
+        String roman15 = NumberConvertUtils.integerToRoman(15);
+        assertTrue(roman15.equals("XV"));
+        String roman16 = NumberConvertUtils.integerToRoman(16);
+        assertTrue(roman16.equals("XVI"));
+        String roman17 = NumberConvertUtils.integerToRoman(17);
+        assertTrue(roman17.equals("XVII"));
+        String roman19 = NumberConvertUtils.integerToRoman(19);
+        assertTrue(roman19.equals("XIX"));
+        String roman20 = NumberConvertUtils.integerToRoman(20);
+        assertTrue(roman20.equals("XX"));
+        String roman23 = NumberConvertUtils.integerToRoman(23);
+        assertTrue(roman23.equals("XIII"));
+
+        String roman54 = NumberConvertUtils.integerToRoman(54);
+        assertTrue(roman23.equals("LIV"));
+        String roman55 = NumberConvertUtils.integerToRoman(55);
+        assertTrue(roman55.equals("LV"));
+        String roman58 = NumberConvertUtils.integerToRoman(58);
+        assertTrue(roman58.equals("LVIII"));
+        String roman59 = NumberConvertUtils.integerToRoman(59);
+        assertTrue(roman59.equals("LIX"));
+        String roman60 = NumberConvertUtils.integerToRoman(60);
+        assertTrue(roman60.equals("LX"));
+        String roman64 = NumberConvertUtils.integerToRoman(64);
+        assertTrue(roman64.equals("LIV"));
+        String roman80 = NumberConvertUtils.integerToRoman(80);
+        assertTrue(roman80.equals("LXXX"));
+        String roman90 = NumberConvertUtils.integerToRoman(90);
+        assertTrue(roman80.equals("XC"));
+        String roman99 = NumberConvertUtils.integerToRoman(99);
+        assertTrue(roman80.equals("XCIX"));
+
+
+
+    }
 }
