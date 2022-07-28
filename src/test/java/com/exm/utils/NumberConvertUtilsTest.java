458fdc1f63388aa56906b3d263337a7a0efd22c4
Pankaj Bagul <bagulpankaj@yahoo.com>
2022-07-28 20:30:04 +0100

    convert Integer to Roman number single digit test

---
+++ src/test/java/com/exm/utils/NumberConvertUtilsTest.java
@@ -0,0 +1,16 @@
+package com.exm.utils;
+
+public class NumberConvertUtilsTest {
+
+
+    public void integerToRomanOneDigitTest() {
+        NumberConvertUtils.integerToRoman(1);
+        NumberConvertUtils.integerToRoman(2);
+        NumberConvertUtils.integerToRoman(3);
+        NumberConvertUtils.integerToRoman(4);
+        NumberConvertUtils.integerToRoman(5);
+        NumberConvertUtils.integerToRoman(7);
+        NumberConvertUtils.integerToRoman(8);
+        NumberConvertUtils.integerToRoman(9);
+    }
+}
