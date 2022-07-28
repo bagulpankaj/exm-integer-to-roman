c50cc7eb9d2af58560b8d1a2b82fe3f8ac7b1eae
Pankaj Bagul <bagulpankaj@yahoo.com>
2022-07-28 20:45:37 +0100

    Added test with asserts

--- src/test/java/com/exm/utils/NumberConvertUtilsTest.java
+++ src/test/java/com/exm/utils/NumberConvertUtilsTest.java
@@ -2,17 +2,29 @@ package com.exm.utils;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class NumberConvertUtilsTest {
 
     @Test
     public void integerToRomanOneDigitTest() {
-        NumberConvertUtils.integerToRoman(1);
-        NumberConvertUtils.integerToRoman(2);
-        NumberConvertUtils.integerToRoman(3);
-        NumberConvertUtils.integerToRoman(4);
-        NumberConvertUtils.integerToRoman(5);
-        NumberConvertUtils.integerToRoman(7);
-        NumberConvertUtils.integerToRoman(8);
-        NumberConvertUtils.integerToRoman(9);
+        String roman1 = NumberConvertUtils.integerToRoman(1);
+        assertTrue(roman1.equals("I"));
+        String roman2 = NumberConvertUtils.integerToRoman(2);
+        assertTrue(roman2.equals("II"));
+        String roman3 = NumberConvertUtils.integerToRoman(3);
+        assertTrue(roman3.equals("III"));
+        String roman4 = NumberConvertUtils.integerToRoman(4);
+        assertTrue(roman4.equals("IV"));
+        String roman5 = NumberConvertUtils.integerToRoman(5);
+        assertTrue(roman5.equals("V"));
+        String roman6 = NumberConvertUtils.integerToRoman(6);
+        assertTrue(roman6.equals("VI"));
+        String roman7 = NumberConvertUtils.integerToRoman(7);
+        assertTrue(roman7.equals("VI"));
+        String roman8 = NumberConvertUtils.integerToRoman(8);
+        assertTrue(roman8.equals("VI"));
+        String roman9 = NumberConvertUtils.integerToRoman(9);
+        assertTrue(roman9.equals("IX"));
     }
 }
